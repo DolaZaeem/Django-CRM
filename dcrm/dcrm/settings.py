ce00c49b37a3ce5b1cd5bb2dfc5b8da9c3fe62a1
DolaZaeem <zaeemdolaofficial07@gmail.com>
2024-02-20 11:33:52 +0530

    Revert "latest"
    This reverts commit 621a0b07c70952a14cf8fe15bc45ba99e8988527.

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'easyaudit',
-    'django_tables2',
 ]
 
 MIDDLEWARE = [
--- dcrm/website/tables.py
+++
@@ -1,7 +0,0 @@
-from django_tables2 import Table, Column
-from .models import Record
-
-class YourModelTable(Table):
-    class Meta:
-        model = Record
-        fields = ('id', 'first_name', 'last_name')
